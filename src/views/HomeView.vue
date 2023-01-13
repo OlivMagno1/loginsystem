57929b7655acb96937a289b63ce8a1afb90cfeb5
OlivMagno <olivmagno1@gmail.com>
2023-01-13 14:08:18 -0300

    logout function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,33 @@
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const Logout = async () => {
+  const res = await fetch("http://localhost:3000/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      session_token: localStorage.getItem("token"),
+    }),
+  }).then((res) => res.json());
+
+  if (res.success) {
+    localStorage.removeItem("token");
+    router.push("/login");
+  } else {
+    alert(res.message);
+  }
+};
+</script>
+
 <template>
   <main>
     <h1>Página Principal</h1>
-    <button>Sair</button>
+    <button @click="Logout">Sair</button>
   </main>
 </template>
-
-<script>
-export default {
-  name: "HomeView",
-};
-</script>
 
 <style scoped>
 main {
