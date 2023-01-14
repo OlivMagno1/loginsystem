432ed39c330397c83043d3c62406a51516a4b915
OlivMagno <olivmagno1@gmail.com>
2023-01-14 17:47:41 -0300

    added a layout for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,17 +24,96 @@ const Logout = async () => {
 
 <template>
   <main>
-    <h1>Página Principal</h1>
-    <button @click="Logout">Sair</button>
+    <header>
+      <h1 class="logo">APLICATIVO</h1>
+      <h2>Menu principal</h2>
+      <p>Aqui estão todas as funcionalidades do nosso aplicativo</p>
+    </header>
+    <div class="pageBody">
+      <span>
+        Muito obrigado por usar nosso aplicativo, atualmente a única
+        funcionalidade existente é sair do sistema.
+      </span>
+      <button @click="Logout">Sair</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
 main {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--primary);
+  color: #fff;
+}
+
+header {
   padding: 1.5rem;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  justify-content: center;
+  width: 35rem;
+}
+
+footer {
+  background-color: #fff;
+  width: 35rem;
+  color: var(--dark);
+  text-align: center;
+  padding: 1.5rem;
+  padding-bottom: 3rem;
+}
+
+h2 {
+  font-size: 2.125rem;
+  margin-bottom: 1rem;
+}
+
+h2 ~ p {
+  font-weight: 500;
+  font-size: 1rem;
+}
+
+.pageBody {
+  flex: 1 1 0%;
+  display: block;
+  border-radius: 1.5rem 1.5rem 0 0;
+  background-color: #fff;
+  box-shadow: 0px -4px 12px 4px rgba(0, 0, 0, 0.16);
+  color: var(--dark);
+  padding: 4rem 1.5rem;
+  width: 35rem;
+}
+
+.pageBody span {
+  display: block;
+  color: var(--gray);
+  font-size: 1rem;
+  font-weight: 500;
+  margin-bottom: 4rem;
 }
 
 button {
-  margin-bottom: 1rem;
+  display: block;
+  width: fit-content;
+  margin: 0 auto;
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: #fff;
+  background-color: var(--primary);
+  padding: 1rem;
+  border: 0;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  transition: 0.2s ease;
+}
+
+button:hover {
+  background-color: var(--primary-dark);
 }
 </style>
