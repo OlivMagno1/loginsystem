d5404682f9985550e9f68fa75dd7cf0963908898
OlivMagno <olivmagno1@gmail.com>
2023-01-19 18:16:11 -0300

    changed from server to serverless functions

---
+++ netlify/functions/register.js
@@ -0,0 +1,57 @@
+exports.handler = async (event, context) => {
+
+  //inicializando Stytch
+  const stytch = require("stytch");
+  const client = new stytch.Client({
+    project_id: process.env.PROJECT_ID,
+    secret: process.env.SECRET,
+    env: stytch.envs.test
+  })
+
+  //headers necessário para habilitar CORS, deve ser retornado junto com o statusCode: 200.
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+  };
+
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 200,
+      headers,
+      body: "This was not a POST request!"
+    };
+  }
+
+  const { email, password } =  JSON.parse(event.body);
+
+  try {
+    const resp = await client.passwords.create({
+      email,
+      password,
+      session_duration_minutes: 15
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        headers,
+        message: "Usuário criado com sucesso",
+        token: resp.session_token
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: false,
+        headers,
+        message: err.error_message,
+        err: err
+      }),
+    };
+  }
+};
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -23,7 +23,7 @@ const Register = async () => {
     return;
   }
 
-  const res = await fetch("http://localhost:3000/register", {
+  const res = await fetch("http://localhost:8888/.netlify/functions/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
